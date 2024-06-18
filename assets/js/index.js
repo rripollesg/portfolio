1887e85f8577fe45cf37de462b2417b1a3cab2b7
Raúl Ripollés <51409066+rripollesg@users.noreply.github.com>
2024-06-18 12:33:51 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -135,7 +135,7 @@ var idiomaNavegador = navigator.language || navigator.userLanguage;
 // Define las URLs para cada idioma
 var urlsPorIdioma = {
   'es': 'https://rripollesg.github.io/portfolio/',
-  'en': 'https://rripollesg.netlify.app/index-EN',
+  'en': 'https://rripollesg.github.io/index-EN',
   'it': 'https://rripollesg.github.io/portfolio/'
 };
 
